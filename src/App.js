82fede473b2b185b850bf28935224aa2f1b08223
rjacob <rahnarjacob@gmail.com>
2024-10-14 22:35:36 +0530

    Adds basic hero section, skills page, navbar + related styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Homescreen/Index';
+import NavBar from './Pages/Home/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-
+      <Router>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
