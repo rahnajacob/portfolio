b1ca5cf750db88f66db2eb6b4f1ee0cb0d29f335
rjacob <rahnarjacob@gmail.com>
2024-10-18 18:54:50 +0530

    adds basic template, minor amount of styling

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,18 @@
+const AboutMe = () => {
+    return (
+        <section id="AboutMe" className="about-section">
+            <div className="about-section-image">
+                <img src="./img/about-me.png" alt="A picture of Rahna" />
+            </div>
+            <div className="hero-section-content-box about-section-box">
+                <div className="hero-section-content">
+                    <p className="section-title">About</p>
+                    <h1 className="skills-section-heading">About Me</h1>
+                    <p className="hero-section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, neque maxime. Temporibus amet sit vitae esse mollitia. Optio repellendus consequatur alias harum, numquam vel repellat magni tenetur beatae quidem. Expedita.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AboutMe
